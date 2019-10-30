335fcbc020e5c5b18ba704098c471ae938b0f5e2
Jagoda Bodnar <jagodabodnar90@gmail.com>
2019-10-30 20:51:44 +0100

    karuzela

---
+++ js/script.js
@@ -0,0 +1,31 @@
+var elem = document.querySelector('.main-carousel');
+var flkty = new Flickity( elem, {
+    freeScroll: true,
+    contain: true,
+    pageDots: false,
+    hash: true
+
+});
+
+var flkty = new Flickity('.main-carousel');
+
+var buttonGroup = document.querySelector('.button-group');
+var buttons = buttonGroup.querySelectorAll('.button');
+buttons = fizzyUIUtils.makeArray( buttons );
+
+buttonGroup.addEventListener( 'click', function( event ) {
+  if ( !matchesSelector( event.target, '.button' ) ) {
+    return;
+  }
+  var index = buttons.indexOf( event.target );
+  flkty.select( index );
+});
+
+var flkty = new Flickity('.main-carousel');
+
+var progressBar = document.querySelector('.progress-bar')
+
+flkty.on( 'scroll', function( progress ) {
+  progress = Math.max( 0, Math.min( 1, progress ) );
+  progressBar.style.width = progress * 100 + '%';
+});
